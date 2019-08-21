f1545016866d8cf1c14ab866cfee16322edddf41
bogomilmihaylov <bogomilmihaylov@live.com>
2019-08-21 17:44:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,12 +18,11 @@ def monopoly_with_third_tier
     railroads:{
       pieces: 4,
       rent_in_dollars: {
-        names: [ ],
+        k1: 1 ,
         k2: 2 ,
         k3: 3 ,
-        k4: 4 
-        
-      }
+        k4: 4  },
+        names: [ ]
     }
   }
 end
